4ed75e49dc6031251ed67f5790b6676eca2c6946
adminL <768418964@qq.com>
2023-06-01 17:18:32 +0800

    Signed-off-by: adminL <768418964@qq.com>

---
+++ js/index_login.js
@@ -0,0 +1,21 @@
+ new Vue({
+        el: '#app',
+
+        data() {
+
+            return {
+                
+            }
+        },
+        mounted: function () {
+            
+        },
+        methods: {
+            handleClose(done) {
+                //编辑对话框关闭
+
+                        done();
+
+            }
+        }
+    })
